f7376cff8e44de9d7eb8e1e2115e484d3aad9d2e
SabrinaPB07 <sabrinapaulabruno@gamil.com>
2024-05-23 18:31:36 -0300

    lista05

---
+++ listas/lista05/js/ex01.js
@@ -0,0 +1,8 @@
+var nome = prompt("digite o seu nome")
+var idade = Number(prompt("Digite a sua idade"))
+var empregoAtual = prompt("digite o seu emprego atual")
+var endereco = prompt("digite o seu endereço")
+
+alert(`ola ${nome} \ntudo bem?`)
+
+alert(`Seu nome é ${nome} \nVocê tem ${idade} anos \nSeu emprego atual é: ${empregoAtual} \nE mora no enereço ${endereco}`)
---
+++ listas/lista05/js/ex02.js
@@ -0,0 +1,6 @@
+var nome = "Max Verstappen"
+var idade = 26
+var empregoAtual = "automobilista"
+var endereco = "Mônaco"
+
+console.log(`Seu nome é ${nome} \nVocê tem ${idade} anos \nSeu emprego atual é: ${empregoAtual} \nE mora no enereço ${endereco}`)

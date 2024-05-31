4eb3ace4b44860508bc17be30b679893ef38ac4a
SabrinaPB07 <sabrinapaulabruno@gamil.com>
2024-05-31 16:49:45 -0300

    lista06

---
+++ listas/lista06/js/ex01.js
@@ -0,0 +1,36 @@
+var pix = {
+    chavePix:'123.456.789-01',
+    valorDaTransferencia: 45.00,
+    dataDeTransacao: '31/05/2024',
+    descricaoPagamento: 'pagando caneca do batman',
+    pagador: [
+        {
+            nome: 'Bruce Wayne',
+            documento: '987.654.321-89'
+        }
+    ],
+    recebedor: [
+        {
+            nome: 'Peter parker',
+            documento: '123.456.789-01'
+        }
+    ],
+    historicoTransacoes: [
+        {
+            data: '31/05/2024',
+            valor: 45.00,
+            descricaoPagamento: 'pagamento caneca do batman',
+        },
+        {
+            data: '20/05/2024',
+            valor: 5000.00,
+            descricaoPagamento: 'salario do Alfred',
+        },
+        {
+            data: '15/05/2024',
+            valor: 60000.00,
+            descricaoPagamento: 'reforma da Batcaverna shhiuuu',
+        },
+    ]
+
+}

a00d87b0048e9e91eda9d27376e17145c13f2723
SabrinaPB07 <sabrinapaulabruno@gmail.com>
2024-06-22 11:30:51 -0300

    aula10

---
+++ aulas/aula10/js/matematica/operacoes.js
@@ -0,0 +1,11 @@
+const somar = (n1, n2) => {
+    console.log(n1 + n2)
+}
+
+const subtracao = (n1, n2) => {
+    console.log(n1 - n2)
+}
+
+export default {
+    somar
+}
---
+++ aulas/aula10/js/script.js
@@ -0,0 +1,6 @@
+// import { somar } from './matematica/operacoes.js'
+//import * as operacoes from './matematica/operacoes.js'
+// import { subtracao as SUb } from './matematica/operacoes.js'
+import teste from './matematica/operacoes'
+
+somar(1, 1)
